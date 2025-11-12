1c59dc33e0e710e503ab3b6f9fbac1b9d56b7008
markh2024 <159157948+markh2024@users.noreply.github.com>
2025-11-12 13:14:42 +0000

    initial commit

---
+++ build/CMakeFiles/age_estimator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/age_estimator.dir/age_estimator.cpp.o"
+  "CMakeFiles/age_estimator.dir/age_estimator.cpp.o.d"
+  "age_estimator"
+  "age_estimator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/age_estimator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
